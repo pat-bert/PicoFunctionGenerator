10eb6c9905e45dda1c5d477ff45a641126b1dd53
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2025-05-31 13:28:28 +0200

    replace submodule by better library for display

---
+++ sh1106.h
@@ -0,0 +1,103 @@
+/**
+ * This Library was originally written by Olivier Van den Eede (4ilo) in 2016.
+ * Some refactoring was done and SPI support was added by Aleksander Alekseev (afiskon) in 2018.
+ *
+ * https://github.com/afiskon/stm32-sh1106
+ */
+
+#ifndef __sh1106_H__
+#define __sh1106_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "hardware/i2c.h"
+
+/* vvv I2C config vvv */
+#ifndef I2C_SDA
+#define I2C_SDA 18
+#endif
+
+#ifndef I2C_SCL
+#define I2C_SCL 19
+#endif
+
+#ifndef I2C_PORT
+#define I2C_PORT		i2c1
+#endif
+
+#ifndef ADDR
+#define ADDR        (0x3C)
+#endif
+/* ^^^ I2C config ^^^ */
+
+// sh1106 maximum height in pixels
+#ifndef SH1106_HEIGHT
+#define SH1106_HEIGHT          64
+#endif
+
+// sh1106 maximum width in pixels
+#ifndef SH1106_WIDTH
+#define SH1106_WIDTH           128
+#endif
+
+#define SSH1106_MAX_WIDTH       132
+#define SSH1106_MAX_HEIGHT      64
+
+// sh1106 maximum width in pixels
+#ifndef SH1106_BUF_SIZE
+#define SH1106_BUF_SIZE           SH1106_HEIGHT * SH1106_WIDTH / 8
+#endif
+
+// Enumeration for screen colors
+typedef enum {
+    Black = 0x00, // Black color, no pixel
+    White = 0x01  // Pixel is set. Color depends on OLED
+} COLOR_t;
+
+// Struct to store transformations
+typedef struct {
+    i2c_inst_t *port;
+    uint8_t addr; 
+    uint8_t col_offset;
+    bool inverted;
+    bool flipped; 
+    bool mirrored; 
+    uint8_t page_buffer[SH1106_WIDTH + 1];
+    uint8_t cmd_buffer[4];
+} sh1106_t;
+
+void sh1106_reset(sh1106_t *disp);
+
+void sh1106_col_start(sh1106_t *disp, uint8_t column);
+void ssh1106_chargepump(sh1106_t *disp, uint8_t value);
+void sh1106_start_line(sh1106_t *disp, uint8_t row);
+void sh1106_contrast(sh1106_t *disp, uint8_t value);
+void sh1106_reverse_cols(sh1106_t *disp, bool mirrored);
+void sh1106_norm_display(sh1106_t *disp, bool norm);
+void sh1106_inverted(sh1106_t *disp, bool invert);
+void sh1106_multiplex(sh1106_t *disp, uint8_t ratio);
+void sh1106_power_on(sh1106_t *disp, bool on);
+void sh1106_display_on(sh1106_t *disp, bool on);
+void sh1106_page(sh1106_t *disp, uint8_t page);
+void sh1106_flipped(sh1106_t *disp, bool flipped);
+void sh1106_vert_offset(sh1106_t *disp, uint8_t offset);
+void sh1106_clk_div(sh1106_t *disp, uint8_t divider, uint8_t freq);
+void sh1106_dis_pre_charge(sh1106_t *disp, uint8_t discharge, uint8_t precharge);
+void sh1106_com_pads(sh1106_t *disp, bool sequential);
+void sh1106_vcomp(sh1106_t *disp, uint8_t level);
+
+int sh1106_write_screen(sh1106_t *disp, uint8_t *buffer);
+int sh1106_write_area(sh1106_t *disp, uint8_t x1, uint8_t x2, uint8_t y1, uint8_t y2, uint8_t *buffer);
+void sh1106_clear_display(sh1106_t *disp);
+
+// bool sh1106_init(sh1106_t *disp, i2c_inst_t *port, uint8_t addr, uint8_t width, uint8_t height, bool inverted, bool flipped, bool mirrored);
+bool sh1106_init(sh1106_t *disp, i2c_inst_t *port, uint8_t addr, uint8_t col_offset);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __sh1106_H__
